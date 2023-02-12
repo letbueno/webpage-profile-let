4807395c67d304c8c367e2b6ff927765117ad28c
letbueno <leticia.bueno.martins@gmail.com>
2023-02-12 17:11:54 -0300

    feat: add icons and just font

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,48 +1,73 @@
 import * as S from "./styles";
 import Me from "../../assets/me.svg";
 import { Badge } from "@chakra-ui/react";
-import Twitter from "../../assets/twitter.svg";
-import Github from "../../assets/github.svg";
-import Linkedin from "../../assets/linkedin.svg";
-import Instagram from "../../assets/instagram.svg";
-import Discord from "../../assets/discord.svg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Profile() {
+  function calculateAge(day: string, month: string, year: string) {
+    const date = new Date();
+    const currentDay = date.getDate();
+    const currentMonth = date.getMonth();
+    const currentYear = date.getFullYear();
+    let age = currentYear - parseInt(year);
+    if (currentMonth < parseInt(month)) {
+      age--;
+    } else if (currentMonth === parseInt(month)) {
+      if (currentDay < parseInt(day)) {
+        age--;
+      }
+    }
+    return age;
+  }
   return (
     <S.Stuff>
       <S.Wrapper>
-        <S.Container>
-          <S.Title>Hi, I'm Leticia</S.Title>
-          <S.Description>I'm brazilian, 22 years</S.Description>
-          <S.Description>I'm Full-stack Developer</S.Description>
-          <S.Description>
-            I'm work at <S.Link href="https://ribon.io/">Ribon</S.Link>
-          </S.Description>
-          <S.Description>I'm graduate in Software Engineer</S.Description>
-          <S.Description>I'm learn new things everday</S.Description>
-        </S.Container>
-        <S.Container>
-          <S.Me src={Me}></S.Me>
-          <S.BadgeContainer>
-            <Badge colorScheme="pink" margin={2}>
-              Love pink stuffs
-            </Badge>
-            <Badge colorScheme="blue" margin={2}>
-              Play LOL
-            </Badge>
-            <Badge colorScheme="purple" margin={2}>
-              Just wear black cloths
-            </Badge>
-          </S.BadgeContainer>
-        </S.Container>
+        <S.Me src={Me}></S.Me>
+        <S.Title>Hi, I'm Leticia</S.Title>
+        <S.BadgeContainer>
+          <Badge colorScheme="pink" margin={2}>
+            Love pink stuffs
+          </Badge>
+          <Badge colorScheme="purple" margin={2}>
+            Play LOL
+          </Badge>
+          <Badge colorScheme="pink" margin={2}>
+            Just wear black cloths
+          </Badge>
+          <Badge colorScheme="purple" margin={2}>
+            Bookstan
+          </Badge>
+        </S.BadgeContainer>
+        <S.Bold>Coordinates:</S.Bold>
+        <S.Description>Tocantins, Brazil</S.Description>
+        <S.Bold>Level:</S.Bold>
+        <S.Description>{calculateAge("20", "10", "1999")}</S.Description>
+        <S.Bold>Ability: </S.Bold>
+        <S.Description>Full-stack Developer</S.Description>
+        <S.Bold>Spawn:</S.Bold>
+        <S.Description>
+          <S.Link href="https://ribon.io/">Ribon</S.Link>
+        </S.Description>
+        <S.Description>letbuenom@icloud.com (copy it)</S.Description>
+        <S.SocialMedia>
+          <S.ContainerIcon href="https://github.com/letbueno">
+            <FontAwesomeIcon icon={["fab", "github"]} size="3x" />
+          </S.ContainerIcon>
+          <S.ContainerIcon href="https://www.linkedin.com/in/leticiabuenom/">
+            <FontAwesomeIcon icon={["fab", "linkedin"]} size="3x" />
+          </S.ContainerIcon>
+          <S.ContainerIcon>
+            <FontAwesomeIcon icon={["fab", "discord"]} size="3x" />
+          </S.ContainerIcon>
+          <S.ContainerIcon href="https://twitter.com/letbuenom">
+            <FontAwesomeIcon icon={["fab", "twitter"]} size="3x" />
+          </S.ContainerIcon>
+          <S.ContainerIcon href="https://www.instagram.com/letbue.no/">
+            <FontAwesomeIcon icon={["fab", "instagram"]} size="3x" />
+          </S.ContainerIcon>
+        </S.SocialMedia>
       </S.Wrapper>
-      <S.SocialMedia>
-        <S.SocialMediaIcon src={Github} />
-        <S.SocialMediaIcon src={Linkedin} />
-        <S.SocialMediaIcon src={Discord} />
-        <S.SocialMediaIcon src={Twitter} />
-        <S.SocialMediaIcon src={Instagram} />
-      </S.SocialMedia>
     </S.Stuff>
   );
 }
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,73 +1,129 @@
-import styled from "styled-components";
+import { Box } from "@chakra-ui/react";
+import styled, { css } from "styled-components";
 
-export const Container = styled.div`
+export const PixelFont = css`
+  font-family: "PF Tempesta Seven Condensed", sans-serif;
+  font-family: "PF Tempesta Seven", sans-serif;
+`;
+export const Container = styled(Box)`
   display: flex;
   flex-direction: column;
   width: 50%;
-  padding: 5%;
 
   a {
     text-decoration: none;
   }
 `;
 
-export const Stuff = styled.div`
+export const Stuff = styled(Box)`
   display: flex;
   flex-direction: column;
   width: 60%;
   background: #e5e5e5;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 2px;
+  border: 3px dashed #9795ef;
   align-items: center;
+  padding: 5%;
+  text-align: center;
 `;
 
-export const Wrapper = styled.div`
+export const Wrapper = styled(Box)`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   height: 100%;
   color: #333;
 `;
 
 export const Title = styled.h1`
+  ${PixelFont}
   font-size: 24px;
-  margin-bottom: 24px;
   font-weight: 600;
+  margin-top: 24px;
+  font-weight: 700;
 `;
 
 export const Description = styled.p`
+  ${PixelFont}
   margin-bottom: 12px;
+  font-size: 12px;
+  font-weight: 400;
 `;
 
 export const Me = styled.img`
   width: 200px;
   height: 200px;
+  border-radius: 50%;
+  border: 3px dashed #9795ef;
+  margin-top: -25%;
+  background: #fff;
 `;
 
 export const BadgeContainer = styled.div`
-  width: 100%;
+  margin: 24px 0;
 `;
 
 export const Link = styled.a`
   color: #00da93;
   font-weight: 600;
+  font-size: 14px;
 `;
 
 export const SocialMedia = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 24px;
+  justify-content: center;
+  margin: 24px 0;
   align-items: center;
+  width: 100%;
+`;
 
-  img {
-    margin-right: 24px;
+export const Bold = styled.p`
+  ${PixelFont}
+  margin-bottom: 6px;
+  font-size: 12px;
+  font-weight: 700;
+`;
+export const SocialMediaIcon = styled.img`
+  height: 40px;
+  width: 40px;
+`;
+
+export const TypeingContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const Typeing = styled.p`
+  overflow: hidden;
+  white-space: nowrap;
+  width: 30em;
+  animation: typing 3s steps(30, end) infinite,
+    blink-caret 0.5s step-end infinite;
+
+  @keyframes typing {
+    from {
+      width: 0;
+    }
+    to {
+      width: 30em;
+    }
+  }
+
+  @keyframes blink-caret {
+    from,
+    to {
+      border-color: transparent;
+    }
+    50% {
+      border-color: black;
+    }
   }
 `;
 
-export const SocialMediaIcon = styled.img`
-  height: 50px;
-  width: 50px;
+export const ContainerIcon = styled.a`
+  margin-right: 24px;
+  cursor: pointer;
 `;
